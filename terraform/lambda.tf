88a8ece55f8abc57496d275efda5f5671da48906
Maartje Eyskens <meyskens@me.com>
2018-04-22 15:33:28 +0100

    Add HTTP endpoint

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -23,6 +23,57 @@ resource "aws_iam_role" "lambda_exec_role" {
 EOF
 }
 
+resource "aws_api_gateway_rest_api" "pager" {
+  name        = "Pager API"
+  description = "Slack to SMS gateway"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.pager.id}"
+  parent_id   = "${aws_api_gateway_rest_api.pager.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.pager.id}"
+  resource_id   = "${aws_api_gateway_rest_api.pager.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.pager.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.pager.invoke_arn}"
+}
+
+
+resource "aws_api_gateway_deployment" "pager" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.pager.id}"
+  stage_name  = "prd"
+}
+
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.pager.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.pager.invoke_arn}"
+}
+
+
+
 resource "aws_lambda_function" "pager" {
     function_name = "pager"
     handler = "pager"
