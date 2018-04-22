73b8c35870846d4717c1526a5e054ddabb68993e
Maartje Eyskens <meyskens@me.com>
2018-04-22 15:46:20 +0100

    change apigw perm

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -79,9 +79,7 @@ resource "aws_lambda_permission" "apigw" {
   function_name = "${aws_lambda_function.pager.arn}"
   principal     = "apigateway.amazonaws.com"
 
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_deployment.pager.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_deployment.pager.execution_arn}/*"
 }
 
 
